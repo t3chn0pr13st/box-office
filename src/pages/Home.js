9b3e0ae65f0553fa991da40aaebafaeb71bc23f0
technopriest <techn@E5-2678-V3>
2020-11-27 18:40:00 +0300

    added search input with button and fetch to api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
 const Home = () => {
+    
+    const [input, setInput] = useState('');
+
+    const onInputChange = event => {
+        setInput(event.target.value);
+    }
+
+    const onSearch = () => {
+        // http://api.tvmaze.com/search/shows?q=girls
+
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+            .then(r => r.json())
+            .then(result => console.log(result));
+
+    }
+
+    const onKeyDown = event => {
+        if (event.keyCode === 13)
+            onSearch();
+    }
+
     return (
         <MainPageLayout>
-            Home page
+            <input type="text" onKeyDown={onKeyDown} onChange={onInputChange} value={input} />
+            <button type="button" onClick={onSearch}>Search</button>
         </MainPageLayout>
     )
 }
 
-export default Home
+export default Home;
