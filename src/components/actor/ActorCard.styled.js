6ecc7eb79e2a0ebab0a154d49b231bfe5587df5f
technopriest <techn@E5-2678-V3>
2020-11-28 02:18:35 +0300

    installed styled components and created styles for search cards

---
+++ src/components/actor/ActorCard.styled.js
@@ -0,0 +1,10 @@
+const { default: styled } = require("styled-components");
+const { SearchCard } = require("../styled");
+
+export const StyledActorCard = styled(SearchCard)`
+  .deathday {
+    margin: 0;
+    margin-top: 15px;
+    font-weight: bold;
+  }
+`;
--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ShowCard from './ShowCard'
 import IMAGE_NOT_FOUND from '../../images/not-found.png'
+import { FlexGrid } from '../styled'
 
 const ShowGrid = ({data}) => {
-    return <div>
+    return <FlexGrid>
         {
             data.map( ({ show }) => (<ShowCard 
                 key={show.id} id={show.id} name={show.name} 
@@ -11,7 +12,7 @@ const ShowGrid = ({data}) => {
                 image={show.image ? show.image.medium : IMAGE_NOT_FOUND} />)
             )
         }
-    </div>
+    </FlexGrid>
 }
 
 export default ShowGrid
