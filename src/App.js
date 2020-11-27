fd5fed22104f572a205a8f265eb03cb1dae95264
technopriest <techn@E5-2678-V3>
2020-11-27 16:59:34 +0300

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      Hi
-    </div>
+    <Switch>
+      <Route path="/" exact>
+        This is home page
+      </Route>
+      <Route path="/starred" exact>
+        This is starred
+      </Route>
+      <Route>
+        This is 404 page
+      </Route>
+    </Switch>
   );
 }
 
