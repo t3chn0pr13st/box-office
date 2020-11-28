76a16cd7280c2beaa7940e770b8b9290fd06b51b
technopriest <techn@E5-2678-V3>
2020-11-28 20:53:22 +0300

    styled show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -6,6 +6,7 @@ import Details from '../components/show/Details';
 import Seasons from '../components/show/Seasons';
 import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
+import { InfoBlock, ShowPageWrapper } from './Show.styled';
 
 const initialState = {
     show: null,
@@ -56,8 +57,6 @@ const Show = () => {
 
     }, [id]);
 
-    console.log('show', show);
-
     if (isLoading)
         return <div>Data is being loaded</div>;
     
@@ -65,21 +64,21 @@ const Show = () => {
         return <div>Error occurred: {error}</div>;
 
     return (
-        <div>
+        <ShowPageWrapper>
             <ShowMainData image={show.image} name={show.name} rating={show.rating} summary={show.summary} tags={show.genres} />
-            <div>
+            <InfoBlock>
                 <h2>Details</h2>
                 <Details status={show.status} network={show.network} premiered={show.premiered} />
-            </div>
-            <div>
+            </InfoBlock>
+            <InfoBlock>
                 <h2>Seasons</h2>
                 <Seasons seasons={show._embedded.seasons} />
-            </div>
-            <div>
+            </InfoBlock>
+            <InfoBlock>
                 <h2>Cast</h2>
                 <Cast cast={show._embedded.cast} />
-            </div>
-        </div>
+            </InfoBlock>
+        </ShowPageWrapper>
     )
 }
 
