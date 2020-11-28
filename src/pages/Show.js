55e935f3221e20ff613a8ec2da2f7350a45ecffb
technopriest <techn@E5-2678-V3>
2020-11-28 15:08:24 +0300

    replaced useState with useReducer on the show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,13 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { useParams } from 'react-router-dom'
 import { apiGet } from '../misc/config';
+
+const initialState = {
+    show: null,
+    isLoading: true,
+    error: null
+}
+
+const reducer = (prevState, action) => {
+    switch (action.type) {
+        case 'FETCH_SUCCESS': {
+            return { ...prevState, isLoading: false, show: action.show, error: null };
+        }
+        case 'FETCH_FAILED': {
+            return { ...prevState, isLoading: false, error: action.error, show: null };
+        }
+        default: return prevState;
+    }
+}
 
 const Show = () => {
 
     const { id } = useParams();
-    const [show, setShow] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+
+    const [{ show, isLoading, error }, dispatch] = useReducer(reducer, initialState);
+    // const [show, setShow] = useState(null);
+    // const [isLoading, setIsLoading] = useState(true);
+    // const [error, setError] = useState(null);
 
     useEffect( () => {
         
@@ -16,13 +36,11 @@ const Show = () => {
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
         .then(result => {
             if (isMounted) {
-                setShow(result);
-                setIsLoading(false);
-            }        
+                dispatch({ type: 'FETCH_SUCCESS', show: result });
+            }
         }).catch(err => {
             if (isMounted) {
-                setError(err.message);
-                setIsLoading(false);
+                dispatch({ type: 'FETCH_FAILED', error: err.message });
             }
         });
 
