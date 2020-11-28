c6d23169f3ba5b7ac5b65f8fc6fecc7f93a5d516
technopriest <techn@E5-2678-V3>
2020-11-29 01:38:43 +0300

    finished starred page

--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
+import { useShows } from '../misc/custom-hooks'
+import { apiGet } from '../misc/config'
+import ShowGrid from '../components/show/ShowGrid'
 
 const Starred = () => {
+
+    const [starred] = useShows();
+
+    const [shows, setShows] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if (starred && starred.length > 0) 
+        {
+            const promises = starred.map(showId => apiGet(`/shows/${showId}`));
+            Promise.all(promises)
+            .then(apiData => apiData.map(show => ({ show })))
+            .then(results => 
+            {
+                setShows(results);
+                setIsLoading(false);
+            }).catch(err => 
+            {
+                setIsLoading(false);
+                setError(err.message);
+            })
+        } else {
+            setIsLoading(false);
+        }
+    }, [starred]);
+
     return (
         <MainPageLayout>
-            This is starred page
+            { isLoading && <div>Loading...</div> }
+            { error && <div>Error: ${error} </div>}
+            { shows && shows.length === 0 && <div>No starred shows yet.</div> }
+            { shows && shows.length > 0 && <ShowGrid data={shows} /> }
         </MainPageLayout>
     )
 }
