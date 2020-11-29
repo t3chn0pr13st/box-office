8f18d1b7cccd5cf2f714f25b841e69e9dc5d84fb
technopriest <techn@E5-2678-V3>
2020-11-29 14:31:53 +0300

    created two new more hooks: useLastQuery and useShow

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,5 @@
-import { useReducer, useEffect } from 'react';
+import { useReducer, useEffect, useState } from 'react';
+import { apiGet } from './config';
 
 function showsReducer(prevState, action) {
     switch (action.type) {
@@ -24,4 +25,66 @@ function usePersistedReducer(reducer, initialState, key)
 
 export function useShows(key = 'shows') {
     return usePersistedReducer(showsReducer, [], key);
+}
+
+export function useLastQuery(key = 'lastQuery') {
+    
+    const [input, setInput] = useState( () => {
+        const persisted = sessionStorage.getItem(key);
+        return persisted ? JSON.parse(persisted) : "";
+    });
+
+    const setPersistedInput = (newState) => {
+        setInput(newState);
+        sessionStorage.setItem(key, JSON.stringify(newState));
+    }
+
+    return [input, setPersistedInput];
+}
+
+const showInitialState = {
+    show: null,
+    isLoading: true,
+    error: null
+}
+
+const showReducer = (prevState, action) => {
+    switch (action.type) {
+        case 'FETCH_SUCCESS': {
+            return { ...prevState, isLoading: false, show: action.show, error: null };
+        }
+        case 'FETCH_FAILED': {
+            return { ...prevState, isLoading: false, error: action.error, show: null };
+        }
+        default: return prevState;
+    }
+}
+
+export function useShow(showId) {
+
+    const [state, dispatch] = useReducer(showReducer, showInitialState);
+    
+    useEffect( () => {
+        
+        let isMounted = true;
+
+        apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`)
+        .then(result => {
+            if (isMounted) {
+                dispatch({ type: 'FETCH_SUCCESS', show: result });
+            }
+        }).catch(err => {
+            if (isMounted) {
+                dispatch({ type: 'FETCH_FAILED', error: err.message });
+            }
+        });
+
+        // возвращенная в этом месте функция вызывается когда компонент выгружается (например при переходе на другую страницу)
+        return () => {
+            isMounted = false;
+        }
+
+    }, [showId]);
+
+    return state;
 }
